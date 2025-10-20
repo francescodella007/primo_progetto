68cee34886b1f0a12a4d4d57be23bdcd2c7ee2b8
francescodella007 <155648739+francescodella007@users.noreply.github.com>
2025-10-20 13:53:15 +0200

    modifica allowe host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(45!6*51c731uafhw-uyzhw89kw5g2^rnpi(!2dhv#7^w$r7j0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
