3a53ea49d2310a34ead4f0f9c12331cfd9aea6ac
francescodella007 <155648739+francescodella007@users.noreply.github.com>
2025-11-10 11:39:24 +0100

    passo 12

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prima_app',
+    'seconda_app'
 ]
 
 MIDDLEWARE = [
@@ -55,9 +56,11 @@ ROOT_URLCONF = 'primo_progetto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates/prima_app'),
-                 os.path.join(BASE_DIR, 'templates'),
-                 ],
+        'DIRS': [
+                os.path.join(BASE_DIR, 'templates'),
+                os.path.join(BASE_DIR, 'prima_app/templates/prima_app'),
+                os.path.join(BASE_DIR, 'seconda_app/templates/seconda_app')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
